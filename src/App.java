89e1e6e7c6b2ce89c56da8564e566c00b653f10b
goatess <107187023+goatess@users.noreply.github.com>
2022-07-08 01:06:24 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,165 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Game game = new Game();
+        game.addPlayers();
+        game.turn();
+    }
+}
+
+class Player {
+    int health = 30;
+    int mana = 0;
+    int manaSlots = 0;
+    int[] damageCards = { 0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 8 };
+    List<Integer> cardsInDeck = new ArrayList<Integer>();
+    List<Integer> cardsInHand = new ArrayList<Integer>();
+
+    void fillCardsInDeck() {
+        for (int i = 0; i < 20; i++) {
+            cardsInDeck.add(damageCards[i]);
+        }
+    }
+
+    final int MIN = 0;
+    int max = 19;
+
+    String drawACard() {
+        String message = "";
+        if (cardsInDeck.size() < 1) {
+            health--;
+            message = "No more cards in deck. BLEEDING OUT! HP-1 ";
+        } else {
+            int randomIndex = (int) Math.floor(Math.random() * (max - MIN + 1) + MIN);
+            int cardDraw = cardsInDeck.get(randomIndex);
+            cardsInDeck.remove(randomIndex);
+            message += cardsInDeck.size() + " Cards in deck.";
+            max--;
+            if (cardsInHand.size() < 5) {
+                cardsInHand.add(cardDraw);
+                message += "A card with a value of " + cardDraw + " drawn.";
+            } else {
+                message += "A card with a value of " + cardDraw + " drawn. OVERLOAD! Card discarded";
+            }
+        } message += cardsInHand.size() + " Cards in hand.";
+        System.out.println(message);
+        return message;
+    }
+
+    void initialSetupPlayer() {
+        fillCardsInDeck();
+        for (int i = 0; i < 3; i++) {
+            drawACard();
+        }
+    }
+
+    void setupActivePlayer() {
+        if (manaSlots < 10) {
+            manaSlots += 1;
+        }
+        mana = manaSlots;
+        drawACard();
+    }
+
+    int playCards() {
+        int totalDamage = 0;
+        String message = "";
+        for (int index = 0; index < cardsInHand.size(); index++) {
+            int cardValue = cardsInHand.get(index);
+            if (mana >= cardValue) {
+                totalDamage += cardValue;
+                mana -= cardValue;
+                cardsInHand.remove(index);
+                message = " throws a card with a value of " + cardValue;
+            } else {
+                message = "PASS TURN";
+                break;
+            }
+            System.out.println(message);
+        }
+        return totalDamage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getManaSlots() {
+        return manaSlots;
+    }
+
+    public int getCardsInHand_size() {
+        int size = cardsInHand.size();
+        return size;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+}
+
+class Game {
+    Player[] player = new Player[2];
+    boolean gameContinues = true;
+    int damage = 0;
+
+    void addPlayers() {
+        for (int index = 0; index < 2; index++) {
+            player[index] = new Player();
+        }
+
+        for (int index = 0; index < 2; index++) {
+            player[index].initialSetupPlayer();
+        }
+    }
+
+    String manageTurn() {
+        int damage = 0;
+        int opponent = 1;
+        int opponentHP = 0;
+        int opponentNewHP = 0;
+        String message = "";
+
+        for (int index = 0; index < 2; index++) {
+            System.out.println("Player " + index);
+            player[index].setupActivePlayer();
+            if (index == 1) {
+                opponent = 0;
+            } else
+            opponent = 1;
+            damage = player[index].playCards();
+            opponentHP = player[opponent].getHealth();
+            opponentNewHP = opponentHP - damage;
+            if (opponentNewHP < 1) {
+                opponentNewHP = 0;
+                gameContinues = false;
+                message += " damages " + opponent;
+                message += " Health drops from " + opponentHP + " to " + opponentNewHP;
+                break;
+            } else {
+                player[opponent].setHealth(opponentNewHP);
+                message += opponent + " gets damaged with " + damage + " points. Health drops from " + opponentHP + " to " + opponentNewHP;
+            }
+            System.out.println(message);
+        }
+        return message;
+    }
+
+    void turn() {
+        while (gameContinues) {
+            manageTurn();
+        }
+
+    }
+
+    void setDamage(int damage) {
+        this.damage = damage;
+    }
+}
---
+++ src/AppTest.java
@@ -0,0 +1,131 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    
+
+    @Test
+    public void each_player_starts_with_30_health(){
+
+        //arrange
+        final int EXPECTED_HEALTH = 30;
+        Player player = new Player();
+        
+        //act
+        int actualHealth = player.getHealth();
+
+        //assert
+        assertEquals(EXPECTED_HEALTH, actualHealth);
+    }
+
+    @Test
+    public void each_player_starts_with_30_healthB(){
+
+        //arrange
+        final int EXPECTED_HEALTH = 30;
+        Game game = new Game();
+        
+        //act
+        game.addPlayers();
+        int actualHealth = game.player[0].getHealth();
+
+        //assert
+        assertEquals(EXPECTED_HEALTH, actualHealth);
+    }
+
+    @Test
+    public void each_player_starts_with_0_manaSlots(){
+
+        //arrange
+        final int EXPECTED_MANASLOTS = 0;
+        Player player = new Player();
+
+        //act
+        int actualManaSlots = player.getManaSlots();
+
+        //assert
+        assertEquals(EXPECTED_MANASLOTS, actualManaSlots);
+    }
+
+
+    @Test
+    public void draw_a_card(){
+        //arrange
+        final int EXPECTED_CARDS_IN_HAND= 4;
+        Game game = new Game();
+        
+        //act
+        game.addPlayers();
+        game.player[0].drawACard();
+        int actualCardsInHand = game.player[0].getCardsInHand_size();
+        
+        //assert
+        assertEquals(EXPECTED_CARDS_IN_HAND, actualCardsInHand);
+    }
+
+    @Test
+    public void  player_receives_3_cards_from_deck_initially(){
+        
+        //arrange
+        final int EXPECTED_CARDS_IN_HAND = 3;        
+        Game game = new Game();
+
+        //act
+        game.addPlayers();
+        int actualCardsInHand = game.player[0].getCardsInHand_size();
+
+        //assert
+        assertEquals(EXPECTED_CARDS_IN_HAND, actualCardsInHand);
+    }
+
+    @Test
+    public void  each_player_receives_3_cards_from_deck_(){
+        
+        //arrange
+        final boolean EXPECTED_SAME_CARDS_IN_HAND = true;        
+        Game game = new Game();
+
+        //act
+        game.addPlayers();
+        int actualCardsInHand = game.player[0].getCardsInHand_size();
+        int actualCardsInHand2 = game.player[1].getCardsInHand_size();
+        boolean sameCardsInHand = actualCardsInHand == actualCardsInHand2;
+
+        //assert
+        assertEquals(EXPECTED_SAME_CARDS_IN_HAND, sameCardsInHand);
+    }
+
+    @Test 
+    public void player_gets_1_more_mana_slot_when_active(){
+        //arrange
+        final int EXPECTED_MANA_SLOTS = 1;
+        Game game = new Game();
+
+        //act
+        game.addPlayers();
+        game.manageTurn();
+        int actualManaSlots = game.player[0].getManaSlots();
+        
+        //assert
+        assertEquals(EXPECTED_MANA_SLOTS, actualManaSlots);
+    }
+
+    @Test 
+    public void active_player_mana_slots_are_refilled(){
+        //arrange
+        final int EXPECTED_MANA = 2;
+        Game game = new Game();
+
+        //act
+        game.addPlayers();
+        game.manageTurn();
+        game.player[0].setupActivePlayer();
+        int actualMana = game.player[0].getMana();
+
+        //assert
+        assertEquals(EXPECTED_MANA, actualMana);
+    }
+
+
+}
